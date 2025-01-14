ff4feb093929b692bcae2358d0f73bf053a248d2
ryfazrin <riansera515@gmail.com>
2025-01-14 16:21:16 +0700

    feat: add rich text react-quill

--- src/components/thread/ThreadInput.jsx
+++ src/components/thread/ThreadInput.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import CreatableSelect from 'react-select/creatable';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import ReactQuill from 'react-quill';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'react-quill/dist/quill.snow.css'; // Import the Quill styles
 
 const options = [
   { value: 'sport', label: 'Sport' },
@@ -29,16 +33,46 @@ function ThreadInput({ addThread }) {
     }
   }
 
-  function handleBodyChange({ target }) {
-    if (target.value.length <= 320) {
-      setBody(target.value);
+  function handleBodyChange(content, delta, source, editor) {
+    const text = editor.getText().trim(); // Get plain text (for character limit validation)
+    if (text.length <= 320) {
+      setBody(content); // Set the rich text content
     }
   }
 
   return (
     <div className="thread-input">
       <input type="text" placeholder="Judul" value={title} onChange={handleTitleChange} />
-      <textarea type="text" placeholder="What are you thinking?" value={body} onChange={handleBodyChange} />
+      <ReactQuill
+        value={body}
+        // eslint-disable-next-line react/jsx-no-bind
+        onChange={handleBodyChange}
+        placeholder="What are you thinking?"
+        modules={{
+          toolbar: [
+            [{ header: '1' }, { header: '2' }, { font: [] }],
+            [{ size: [] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            ['link', 'image'],
+            ['clean'],
+          ],
+        }}
+        formats={[
+          'header',
+          'font',
+          'size',
+          'bold',
+          'italic',
+          'underline',
+          'strike',
+          'blockquote',
+          'list',
+          'bullet',
+          'link',
+          'image',
+        ]}
+      />
       <p className="thread-input__char-left">
         <strong>{body.length}</strong>
         /320
