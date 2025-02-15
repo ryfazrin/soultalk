90b3bccbd798326b0c55c5b07a4a17a2d90d4b64
ryfazrin <riansera515@gmail.com>
2025-02-15 12:26:36 +0700

    feat: add utils month & year

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,18 +3,29 @@ function postedAt(date) {
   const posted = new Date(date);
   const diff = now - posted;
   const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const diffMonths = Math.floor(diffDays / 30); // Asumsikan rata-rata 30 hari dalam sebulan
+  const diffYears = Math.floor(diffDays / 365); // Asumsikan 365 hari dalam setahun
   const diffHours = Math.floor(diff / (1000 * 60 * 60));
   const diffMinutes = Math.floor(diff / (1000 * 60));
   const diffSeconds = Math.floor(diff / 1000);
 
+  if (diffYears > 0) {
+    return `${diffYears} year${diffYears > 1 ? 's' : ''} ago`;
+  }
+  if (diffMonths > 0) {
+    return `${diffMonths} month${diffMonths > 1 ? 's' : ''} ago`;
+  }
   if (diffDays > 0) {
-    return `${diffDays} days ago`;
-  } if (diffHours > 0) {
-    return `${diffHours} hours ago`;
-  } if (diffMinutes > 0) {
-    return `${diffMinutes} minutes ago`;
-  } if (diffSeconds > 0) {
-    return `${diffSeconds} seconds ago`;
+    return `${diffDays} day${diffDays > 1 ? 's' : ''} ago`;
+  }
+  if (diffHours > 0) {
+    return `${diffHours} hour${diffHours > 1 ? 's' : ''} ago`;
+  }
+  if (diffMinutes > 0) {
+    return `${diffMinutes} minute${diffMinutes > 1 ? 's' : ''} ago`;
+  }
+  if (diffSeconds > 0) {
+    return `${diffSeconds} second${diffSeconds > 1 ? 's' : ''} ago`;
   }
   return 'just now';
 }
